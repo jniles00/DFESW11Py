4888c0b04f7e1e3cf5e8056c115bb773b346de44
jorden-niles <jordenniles@gmail.com>
2022-02-08 09:15:55 +0000

    JN: Trying to add DataTypes and Pracitce py again

---
+++ DataTypes.py
@@ -0,0 +1,28 @@
+# word1 = "Good"
+# word2 = "Day"
+# word3 = "Jimothy"
+
+# sentence = word1 + " " + word2 + ", " + word3
+
+# print(sentence)
+
+# number1 = input("Enter whole number: ")
+# number2 = input("Enter decimal number: ")
+
+# integer_number = int(number1)   # creates a whole number
+# float_number = float(number2)   # creates a number with at least one decimal point
+# round_number = int(round(float_number)) # rounds the float to the nearest whole number and then converts it
+
+# print(number1)
+# print(number2)
+# print(round_number)
+
+
+name = "Pep Guardogiola"
+age = 3
+bark = True
+tweet = False
+
+print("My pet is called", name, ", He is", age, "years old.")    # concatenating the variables into a string
+print("Statement:", name, "barks", bark)
+print("Statement:", name, "tweets", tweet)
---
+++ Practice.py
@@ -0,0 +1,35 @@
+# # string to integer conversion
+
+# strToInt = "1337"
+# print(type(strToInt))
+
+# print(strToInt)
+
+# strToInt = int(strToInt)
+# print(type(strToInt))
+
+# print(strToInt)
+
+# # bool to string
+
+# boolToStr = True
+# print(boolToStr)
+
+# print(type(boolToStr))
+
+# boolToStr = str(boolToStr)
+# print(type(boolToStr))
+
+# print(boolToStr)
+
+# int to boolean
+
+intToBool = input("Enter a whole number: ")
+print(intToBool)
+
+print(type(intToBool))
+
+intToBool = bool(intToBool)
+print(type(intToBool))
+
+print(intToBool)
